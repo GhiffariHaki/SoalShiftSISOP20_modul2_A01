17f0221f8db7407500bae670557f0792fe66de07
Haki <57068224+GhiffariHaki@users.noreply.github.com>
2020-03-13 03:32:21 +0700

    Add files via upload

---
+++ soal1/bashsoal1.sh
@@ -0,0 +1,2 @@
+sekarang=`date`
+echo "Sukses, $sekarang"
---
+++ soal1/soal1.c
@@ -0,0 +1,131 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+#include <wait.h>
+
+int cekdigit(char* input){
+  int i=0;
+  for(i; i < strlen(input); i++){
+    if(isalpha(input[i])) return -1;
+  }
+  return 1;
+}
+
+int main(int argc, char *argv[]) {
+  int jam,menit,detik;
+  // Mengecek apabila argumen sama dengan 4
+  if(argc != 5){ //Argumen terakhir untuk NULL
+    printf("Harus 4 Argumen!!!\n");
+    return 0;
+  }
+
+  // Mengecek apabila detik sesuai argumen yang diinginkan
+  if(strcmp(argv[1],"*") == 0) detik = 60;
+  else if(cekdigit(argv[1])){
+    printf("Harus Input Angka 0-59!!\n");
+    return 0;
+  }
+  else{
+    detik = atoi(argv[1]);
+    if(detik > 59 || detik < 0){
+      printf("Harus Input Angka 0-59!!\n");
+      return 0;
+    }
+  }
+
+  // Mengecek apabila menit sesuai argumen yang diinginkan
+  if(strcmp(argv[2],"*") == 0) menit = 60;
+  else if(cekdigit(argv[2])){
+    printf("Harus Input Angka 0-59!!\n");
+    return 0;
+  }
+  else{
+    menit = atoi(argv[2]);
+    if(menit > 59 || menit < 0){
+      printf("Harus Input Angka 0-59!!\n");
+      return 0;
+    }
+  }
+
+
+  // Mengecek apabila jam sesuai argumen yang diinginkan
+  if(strcmp(argv[3],"*") == 0) jam = 24;
+  else if(cekdigit(argv[3])){
+    printf("Harus Input Angka 0-23!!\n");
+    return 0;
+  }
+  else{
+    jam = atoi(argv[3]);
+    if(jam > 23 || jam < 0){
+      printf("Harus Input Angka 0-23!!\n");
+      return 0;
+    }
+  }
+
+  //printf("%d, %d, %d, %s\n", detik, menit, jam, argv[4]);
+
+  // BEGIN CONTOH IMPLEMENTASI DAEMON MODUL
+  pid_t pid, sid;        // Variabel untuk menyimpan PID
+
+  pid = fork();     // Menyimpan PID dari Child Process
+
+  /* Keluar saat fork gagal
+  * (nilai variabel pid < 0) */
+  if (pid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  /* Keluar saat fork berhasil
+  * (nilai variabel pid adalah PID dari child process) */
+  if (pid > 0) {
+    exit(EXIT_SUCCESS);
+  }
+
+  umask(0);
+
+  sid = setsid();
+  if (sid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+  // END CONTOH IMPLEMENTASI DAEMON MODUL
+
+  while (1) {
+    int status;
+    time_t rawtime;
+    struct tm*info = localtime(&rawtime);
+    char buffer[80];
+
+    info = localtime(&rawtime);
+    // Menyimpan info waktu sekarang dalam variabel buffer
+    strftime(buffer,80,"%Y-%m-%d_%H:%M:%S", info);
+    // Print Jam sekarang
+    printf("Jam Sekarang : %s",buffer);
+
+    //Isi Struct tm : tutorialspoint.com/c_standard_library/c_function_localtime.htm
+    if((info->tm_sec == detik || detik == 60) && (info->tm_min == menit || menit == 60) && (info->tm_hour == jam || jam == 24)){
+	pid_t child_id;
+	child_id = fork();
+
+	if(child_id == 0){
+	  //CHILD
+	  char *bashscr[] = {"bash", argv[4], NULL};
+	  execv("/usr/bin/bash", bashscr);
+	}
+	else while ((wait(&status)) > 0);
+	//Mempunyai Jarak 1 detik
+	sleep(1);
+    }
+  }
+}
